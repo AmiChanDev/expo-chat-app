3c9d96b23ebeca46080fe58230601342976f7281
AmiChanDev <gamageamantha@gmail.com>
2025-09-16 20:49:28 +0530

    Refactor navigation structure and add missing screens: Home, Profile, SignIn, SignUp, and Setting. Replace Splash component with SplashScreen and update imports accordingly.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import "./global.css";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from "./src/screens/Splash";
+
+//screens
+import SplashScreen from "./src/screens/SplashScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import SignInScreen from "./src/screens/SignInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import SettingScreen from "./src/screens/SettingScreen";
+
 import { NavigationContainer } from "@react-navigation/native";
 
 type rootStackParamList = {
-  Splash: undefined;
+  SplashScreen: undefined;
+  HomeScreen: undefined;
+  ProfileScreen: undefined;
+  SignInScreen: undefined;
+  SignUpScreen: undefined;
+  SettingScreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<rootStackParamList>();
@@ -12,8 +25,13 @@ const Stack = createNativeStackNavigator<rootStackParamList>();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Splash" component={SplashScreen} />
+      <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="SignInScreen" component={SignInScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="SettingScreen" component={SettingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
